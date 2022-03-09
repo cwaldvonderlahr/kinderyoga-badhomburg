b1eaeb28ab041fe67b353ffdde580e45ea193e6d
Christian Wald-von der Lahr <mail@waldvonderlahr-it.de>
2022-03-09 23:47:45 +0100

    useMeta remove

--- app.vue
+++ app.vue
@@ -6,16 +6,3 @@
   </div>
 </template>
 
-<script setup>
-useMeta({
-  title: "Kinderyoga Bad Homburg",
-  meta: [
-    {
-      hid: "description",
-      name: "description",
-      content: "Kinderyoga Bad Homburg",
-    },
-    { hid: "robots", name: "robots", content: "noindex" },
-  ],
-});
-</script>
