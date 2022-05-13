99ab7a35a645fc8a9ba728dafc8adc34b686582a
Christian Wald-von der Lahr <mail@waldvonderlahr-it.de>
2022-05-13 21:07:30 +0200

    Update Imprint

--- components/Imprint.vue
+++ components/Imprint.vue
@@ -1,22 +1,26 @@
 <template>
-  <div class="relative bg-white">
+  <div class="relative bg-yoga-lightyellow">
     <div class="relative max-w-7xl mx-auto py-4 lg:py-14 px-4 sm:px-6 lg:px-8">
-      <div class="mt-8">
-        <div class="mt-8 lg:mt-0">
-          <div class="max-w-prose mx-auto lg:max-w-none">
-            <h2
-              class="font-chalkduster text-2xl font-extrabold tracking-tight text-yoga-brand sm:text-3xl"
-            >
-              Impressum
-            </h2>
-            <p class="mt-8 text-lg leading-6 text-gray-500">
-             Hier entsteht gerade ein wunderschönes Impressum.
-            </p>
+      <div class="mt-8 lg:mt-0">
+        <div class="max-w-prose mx-auto lg:max-w-none">
+          <h1 class="font-chalkduster text-2xl font-extrabold tracking-tight text-yoga-brand sm:text-3xl">Impressum</h1>
 
-            <p class="mt-8 text-lg leading-6 text-gray-500">
-            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
-            </p>
-          </div>
+          <h2 class="mt-8 text-xl leading-6 text-gray-700">Angaben gem&auml;&szlig; &sect; 5 TMG</h2>
+          <p class="mt-8 text-lg leading-6 text-gray-500">Claudia R&ouml;ske<br />
+          Kinderyoga Claudia R&ouml;ske<br />
+          H&ouml;hestrasse 5<br />
+          61348 Bad Homburg</p>
+
+          <h2 class="mt-8 text-xl leading-6 text-gray-700">Kontakt</h2>
+          <p class="mt-8 text-lg leading-6 text-gray-500">E-Mail: Claudia@kinderyoga-badhomburg.de</p>
+
+          <h2 class="mt-8 text-xl leading-6 text-gray-700">EU-Streitschlichtung</h2>
+          <p class="mt-8 text-lg leading-6 text-gray-500">Die Europ&auml;ische Kommission stellt eine Plattform zur Online-Streitbeilegung (OS) bereit: <a href="https://ec.europa.eu/consumers/odr/" target="_blank" rel="noopener noreferrer">https://ec.europa.eu/consumers/odr/</a>.<br /> Unsere E-Mail-Adresse finden Sie oben im Impressum.</p>
+
+          <h2 class="mt-8  text-xl leading-6 text-gray-700">Verbraucher&shy;streit&shy;beilegung/Universal&shy;schlichtungs&shy;stelle</h2>
+          <p class="mt-8 text-lg leading-6 text-gray-500">Wir sind nicht bereit oder verpflichtet, an Streitbeilegungsverfahren vor einer Verbraucherschlichtungsstelle teilzunehmen.</p>
+
+          <p class="mt-8 text-lg leading-6 text-gray-500">Quelle: <a href="https://www.e-recht24.de">eRecht24</a></p>
         </div>
       </div>
     </div>
