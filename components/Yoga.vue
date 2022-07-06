51797aa740d1ce21f81b52fa687452e8ebcf8ae2
Christian Wald-von der Lahr <mail@waldvonderlahr-it.de>
2022-07-06 15:53:52 +0200

    Optimize Images and WebP

--- components/Yoga.vue
+++ components/Yoga.vue
@@ -6,13 +6,11 @@
           <div class="relative text-base mx-auto max-w-prose lg:max-w-none">
             <figure>
               <div class="aspect-w-12 aspect-h-7 lg:aspect-none">
-                <img
-                  class="rounded-lg shadow-lg object-cover object-center"
-                  src="~/assets/img/strand.jpg"
-                  alt="Kinderyoga am Strand"
-                  width="655"
-                  height="382"
-                />
+                <picture class="rounded-lg shadow-lg object-cover object-center">
+                  <source srcset="~/assets/img/strand.webp 1x, ~/assets/img/strand@2x.webp 2x" type="image/webp" width="655" height="382">
+                  <source srcset="~/assets/img/strand.jpg 1x, ~/assets/img/strand@2x.jpg 2x"  width="655" height="382">
+                  <img src="~/assets/img/strand.jpg" alt="Kinderyoga am Strand"  width="655" height="382">
+                </picture>
               </div>
             </figure>
           </div>
